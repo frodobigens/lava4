5789d2b7f2731e4a29659b46a0b0fad451d3c691
Frodo <83783418+frodobigens@users.noreply.github.com>
2024-05-16 23:52:41 +0300

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-dd239382-e149-4c48-88f8-a2c00b95910e/"
 
 OUTPUT_DIR="logs"
 
-eth_data=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","id":1}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+eth_data=$(curl -A UA -x http://104.238.9.158:6611 --proxy-user cwmgurpo:3lv7ii9r7e6p -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","id":1}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
